f7819e2d4901d35f4ef8976aafe4eeebcc6059d2
Kevin Hugh <KHugh@Kevins-MacBook-Pro-2.local>
2016-11-18 23:39:49 -0500

    fixed bouncing for random directions

--- MurderJosh/Assets/Scripts/bulletController.cs
+++ MurderJosh/Assets/Scripts/bulletController.cs
@@ -5,14 +5,17 @@ public class bulletController : MonoBehaviour {
 	public Rigidbody2D rb;
 	private Vector2 oldVelocity;
 
-	public float fireSpeed = 2f;
+	public float fireSpeed = 8f;
 
 	// Use this for initialization
 	void Start () {
 		// set our laser on its merry way. no need to update transform manually
-		rb.velocity = new Vector2(1,2) * fireSpeed;
+		// Initialise with a random vector, normalize it then multiply by bullet speed
+		Vector2 initVector = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f)) ;
+		initVector.Normalize();
+		initVector = initVector * fireSpeed;
 
-		Debug.Log (rb.velocity);
+		rb.velocity = initVector;
 
 		// freeze the rotation so it doesnt go spinning after a collision
 		rb.freezeRotation = true;
@@ -34,19 +37,26 @@ public class bulletController : MonoBehaviour {
 
 	// when a collision happens
 	void OnCollisionEnter2D (Collision2D collision) {
-		// get the point of contact
-		ContactPoint2D contact = collision.contacts[0];
 
-		// reflect our old velocity off the contact point's normal vector
-		Vector2 reflectedVelocity = Vector2.Reflect(oldVelocity, contact.normal);        
 
-		// assign the reflected velocity back to the rigidbody
-		rb.velocity = reflectedVelocity;
-		// rotate the object by the same ammount we changed its velocity
-		Quaternion rotation = Quaternion.FromToRotation(oldVelocity, reflectedVelocity);
-		transform.rotation = rotation * transform.rotation;
+			Debug.Log ("collided with NON bullet");
+
+
+			// get the point of contact
+			ContactPoint2D contact = collision.contacts [0];
+
+			// reflect our old velocity off the contact point's normal vector
+			Vector2 reflectedVelocity = Vector2.Reflect (oldVelocity, contact.normal);        
+
+			// assign the reflected velocity back to the rigidbody
+			rb.velocity = reflectedVelocity;
+			// rotate the object by the same ammount we changed its velocity
+			Quaternion rotation = Quaternion.FromToRotation (oldVelocity, reflectedVelocity);
+			transform.rotation = rotation * transform.rotation;
+
+		}
 	}
 
-}
+
 
 
