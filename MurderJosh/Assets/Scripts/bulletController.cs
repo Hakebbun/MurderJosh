47a8ed76740d33b2569345fdb7568526bb4fede6
Kevin Hugh <KHugh@Kevins-MacBook-Pro-2.local>
2016-11-19 01:09:23 -0500

    code cleaning

--- MurderJosh/Assets/Scripts/bulletController.cs
+++ MurderJosh/Assets/Scripts/bulletController.cs
@@ -9,12 +9,6 @@ public class bulletController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-//		// set our laser on its merry way. no need to update transform manually
-//		// Initialise with a random vector, normalize it then multiply by bullet speed
-//		Vector2 initVector = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f)) ;
-//		initVector.Normalize();
-//		initVector = initVector;
-
 		rb.velocity = rb.velocity  * fireSpeed;
 
 		// freeze the rotation so it doesnt go spinning after a collision
