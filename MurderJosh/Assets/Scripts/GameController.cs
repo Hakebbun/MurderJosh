4534d8151166796c66e3bc922da80c36edbb3012
mahtabsabet <mahtabz@gmail.com>
2016-11-19 01:23:28 -0500

    Added more text, and updated the score math

--- MurderJosh/Assets/Scripts/GameController.cs
+++ MurderJosh/Assets/Scripts/GameController.cs
@@ -1,26 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
 	private float score;
 	private float scoreMultiplier;
+	public bool gameOver;											//TODO: make this private
+	private bool restart;
+
 
 	public int numberOfBullets;										//TODO: This is just for testing purposes. For the actual game get this number for real
 	public Text scoreText;
+	public Text gameOverText;
+	public Text restartText;
+	public static GameController instance = null;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+	}
 
 	// Use this for initialization
 	void Start () {
-		updateScoreText ();
+		scoreText.text = "Score: " + score;
+		gameOverText.text = "";
+		restartText.text = "";
 		score = 0;
 		InvokeRepeating("UpdateEverySecond", 0f, 1.0f);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		scoreMultiplier = Mathf.Log10 (10 * numberOfBullets);
+		scoreMultiplier = Mathf.Pow (numberOfBullets, 2);
 		Debug.Log ("Score Multiplier: " + scoreMultiplier);
+		if (gameOver) {
+			restartText.text = "Press 'R' to Restart";
+			restart = true;
+		}
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				int scene = SceneManager.GetActiveScene ().buildIndex;
+				SceneManager.LoadScene (scene, LoadSceneMode.Single);
+			}
+		}
 	}
 
 	/// <summary>
@@ -37,6 +66,14 @@ public class GameController : MonoBehaviour {
 	/// </summary>
 	void updateScoreText(){
 		scoreText.text = "Score: " + score;
+	}
+
+	/// <summary>
+	/// Games the over.
+	/// </summary>
+	void GameOver(){
+		gameOverText.text = "Game Over";
+		gameOver = true;
 	}
 
 }
